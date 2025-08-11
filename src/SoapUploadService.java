10103bd69d6fbe0b5373c2a265ffdabf1ae25d08
Ghazal2623 <ghazal.ashraf24@gmail.com>
2025-08-12 00:14:59 +0400

    Update SoapUploadService.java

--- src/SoapUploadService.java
+++ src/SoapUploadService.java
@@ -1,1 +1,14 @@
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService
+public class SoapUploadService {
+
+    @WebMethod
+    public String uploadSignature(byte[] signatureData) {
+        // Simulate processing signature upload
+        System.out.println("Received signature data of length: " + (signatureData != null ? signatureData.length : 0));
+        return "Signature uploaded successfully";
+    }
+}
 
